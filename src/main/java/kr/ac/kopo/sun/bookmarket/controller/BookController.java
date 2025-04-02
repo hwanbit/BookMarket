a6f5684e28e544c2ea54f51786d17a6bf20c14a2
Hwanbit <hwanbit@gmail.com>
2025-04-02 11:42:58 +0900

    BookMarket의 Controller를 다양한 방법으로 수정

--- src/main/java/kr/ac/kopo/sun/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/sun/bookmarket/controller/BookController.java
@@ -5,20 +5,33 @@ import kr.ac.kopo.sun.bookmarket.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
+@RequestMapping(value = "/books")
 public class BookController {
     @Autowired
     private BookService bookService;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    @GetMapping
     public String requestBookList(Model model) {
         List<Book> bookList = bookService.getAllBookList();
         model.addAttribute("bookList", bookList);
         return "books"; // templates 이름과 동일
+    }
+
+    @GetMapping("/all")
+//    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public ModelAndView requestAllBookList() {
+        ModelAndView modelV = new ModelAndView();
+        modelV.setViewName("books");
+        List<Book> bookList = bookService.getAllBookList();
+        modelV.addObject("bookList", bookList);
+        return modelV; // ModelAndView 이름과 동일
     }
 }
