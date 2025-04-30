03702a2bc77c7ce85587f515cdb545598b80a9c2
Hwanbit <hwanbit@gmail.com>
2025-04-30 11:42:53 +0900

    BookMarket의 6장 도서 등록 페이지 완성

--- src/main/java/kr/ac/kopo/sun/bookmarket/repository/BookRepository.java
+++ src/main/java/kr/ac/kopo/sun/bookmarket/repository/BookRepository.java
@@ -14,4 +14,6 @@ public interface BookRepository {
     List<Book> getBookListByCategory(String category);
 
     Set<Book> getBookListByFilter(Map<String, List<String>> filter);
+
+    void setNewBook(Book book);
 }
