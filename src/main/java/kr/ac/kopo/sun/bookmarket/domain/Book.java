a1189bd373bfd6376bd408ce8073f3a5b881380c
Hwanbit <hwanbit@gmail.com>
2025-05-14 11:33:16 +0900

    BookMarket의 9장 유효성검사 Bean Validation

--- src/main/java/kr/ac/kopo/sun/bookmarket/domain/Book.java
+++ src/main/java/kr/ac/kopo/sun/bookmarket/domain/Book.java
@@ -1,6 +1,7 @@
 package kr.ac.kopo.sun.bookmarket.domain;
 
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,13 @@ import java.math.BigDecimal;
 //@Setter
 //@NoArgsConstructor
 public class Book {
+    @Pattern(regexp = "isbn[0-9]+")
     private String bookId; //도서번호
+    @Size(min = 4, max = 50)
     private String name; //도서명
+    @Min(0)
+    @Digits(integer = 8, fraction = 2)
+    @NotNull
     private BigDecimal unitPrice; //단가
     private String author; //저자
     private String description; //도서설명
