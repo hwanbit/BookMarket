3de24c1a3aca37c1cc6841e92324a493be9ba9a6
Hwanbit <hwanbit@gmail.com>
2025-05-28 13:09:00 +0900

    BookMarket의 10장 Security (도서등록)

---
+++ src/main/java/kr/ac/kopo/sun/bookmarket/controller/LoginController.java
@@ -0,0 +1,26 @@
+package kr.ac.kopo.sun.bookmarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/loginfailed")
+    public String loginFailed(Model model) {
+        model.addAttribute("error", "true");
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "login";
+    }
+
+}
